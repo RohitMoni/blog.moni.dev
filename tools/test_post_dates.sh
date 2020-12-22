a77a761adf710185ebb645a31fb82c7a6c2be26e
Rohit Moni <rohitmdxb@gmail.com>
2020-12-22 06:08:43 +0000

    Test iterations

--- tools/test_post_dates.sh
+++ tools/test_post_dates.sh
@@ -3,6 +3,8 @@
 # Test to ensure that post filenames' dates match their layout date for org purposes
 # (This is their date created)
 
-for filename in /_posts/*.md; do
+for filename in _posts/*.md; do
+    [ -e "$filename" ] || continue
+    # ... rest of the loop body
     echo "$filename"
 done
